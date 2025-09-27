6df086e1c4f8216f8e4b23e126cf444d982f81a4
GetCreativeMalhi <108103415+GetCreativeMalhi@users.noreply.github.com>
2025-09-28 09:51:47 +1000

    feat: add logic to create grid of divs
    -function added to create grid of divs
    -grid of divs change color when mouse goes over
    -CSS added for container and grid divs
    -HTML tags included for button and heading

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
-alert("Doodle Box");
+const gridContainer = document.querySelector('.gridContainer');
+const rows = 100;
+const columns = 100;
+
+
+for(let i = 0; i < rows; i++){
+
+    for (let j = 0; j < columns; j++){
+        const gridItem = document.createElement('div');
+
+        gridItem.className = 'grid-item';
+        gridItem.addEventListener('mouseenter', (e) => {
+            gridItem.style.backgroundColor = "#444444";
+        })
+        
+        gridContainer.appendChild(gridItem);
+    }
+
+}
