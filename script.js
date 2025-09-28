06db95f7b18347bda2514946f04f7d307ad2daf8
GetCreativeMalhi <108103415+GetCreativeMalhi@users.noreply.github.com>
2025-09-28 10:58:28 +1000

    feat: add button listener to get the user input
    -user will enter the gridSize through prompt

--- script.js
+++ script.js
@@ -1,19 +1,26 @@
 const gridContainer = document.querySelector('.gridContainer');
-const rows = 100;
-const columns = 100;
-
-
-for(let i = 0; i < rows; i++){
-
-    for (let j = 0; j < columns; j++){
-        const gridItem = document.createElement('div');
-
-        gridItem.className = 'grid-item';
-        gridItem.addEventListener('mouseenter', (e) => {
-            gridItem.style.backgroundColor = "#444444";
-        })
-        
-        gridContainer.appendChild(gridItem);
+const gridSizePromptButton = document.querySelector('button');
+let gridSize = 0;
+gridSizePromptButton.addEventListener('click', () => {
+    gridSize = +(prompt("Enter Grid Size (Please Enter a number between 1 and 100)", "64"));
+    while(gridSize <= 0 || gridSize > 100){
+        gridSize = +(prompt("Enter Grid Size (Please Enter a number between 1 and 100)", "64"));
     }
 
-}
+    console.log(gridSize);
+    for(let i = 0; i < gridSize; i++){
+        console.log(gridSize);
+        for (let j = 0; j < gridSize; j++){
+            const gridItem = document.createElement('div');
+
+            gridItem.className = 'grid-item';
+            gridItem.addEventListener('mouseenter', (e) => {
+                gridItem.style.backgroundColor = "#444444";
+                // gridItem.style.setProperty('--grid-size', gridSize);
+            });
+        
+            gridContainer.appendChild(gridItem);
+        }
+
+    }
+});
