59a0f181e2006647680ee709990e6d59997af8e5
GetCreativeMalhi <108103415+GetCreativeMalhi@users.noreply.github.com>
2025-09-30 11:25:11 +1000

    feat:add darken the color 10% for every mouseover
    -switch statements added to handle brightness of
    grids which decreases when mouse enters the
    elements, max 10 visits and it goes dark.

--- script.js
+++ script.js
@@ -1,18 +1,19 @@
 const gridContainer = document.querySelector('.gridContainer');
 const gridSizePromptButton = document.querySelector('button');
 let gridSize = 0;
+
 function getRandomNumber(min, max){
     const minCeiled = Math.ceil(min);
     const maxFloored = Math.floor(max);
     return Math.floor(Math.random()* (maxFloored - minCeiled) + minCeiled);
 }
+
 gridSizePromptButton.addEventListener('click', () => {
     const removeChild = document.querySelectorAll('.gridContainer div');
     
     removeChild.forEach((child) => {
         gridContainer.removeChild(child);
     })
-    console.log(removeChild);
     gridSize = +(prompt("Enter Grid Size (Please Enter a number between 1 and 100)", "64"));
     while(gridSize <= 0 || gridSize > 100){
         gridSize = +(prompt("Enter Grid Size (Please Enter a number between 1 and 100)", "64"));
@@ -24,18 +25,63 @@ gridSizePromptButton.addEventListener('click', () => {
         for (let j = 0; j < gridSize; j++){
             const gridItem = document.createElement('div');
             gridItem.className = 'grid-item';
+            let mouseEnterCounter = 0;
             gridItem.addEventListener('mouseenter', (e) => {
                 let redColor = getRandomNumber(1, 255);
                 let greenColor = getRandomNumber(1, 255);
                 let blueColor = getRandomNumber(1, 255);
-                
-                console.log(`rgb(${redColor}, ${greenColor}, ${blueColor})`);
-                gridItem.style.backgroundColor = `rgb(${redColor}, ${greenColor}, ${blueColor})`;
-                // gridItem.style.setProperty('--grid-size', gridSize);
+                mouseEnterCounter++;
+                const gridItemColor = `rgb(${redColor}, ${greenColor}, ${blueColor})`;
+                gridItem.style.backgroundColor = gridItemColor;
+                switch(mouseEnterCounter){
+                    case 1:
+                        gridItem.style.filter = `brightness(${100 - (mouseEnterCounter * 10)}%)`;
+                        break;
+                    case 2:
+                        
+                        gridItem.style.filter = `brightness(${100 - (mouseEnterCounter * 10)}%)`;
+                        break;
+                    case 3:
+                       
+                        gridItem.style.filter = `brightness(${100 - (mouseEnterCounter * 10)}%)`;
+                        break;
+                    case 4:
+                        
+                        gridItem.style.filter = `brightness(${100 - (mouseEnterCounter * 10)}%)`;
+                        break;
+                    case 5:
+                       
+                        gridItem.style.filter = `brightness(${100 - (mouseEnterCounter * 10)}%)`;
+                        break;
+                    case 6:
+                        
+                        gridItem.style.filter = `brightness(${100 - (mouseEnterCounter * 10)}%)`;
+                        break;
+                    case 7:
+                        
+                        gridItem.style.filter = `brightness(${100 - (mouseEnterCounter * 10)}%)`;
+                        break;
+                    case 8:
+                        
+                        gridItem.style.filter = `brightness(${100 - (mouseEnterCounter * 10)}%)`;
+                        break;
+                    case 9:
+                       
+                        gridItem.style.filter = `brightness(${100 - (mouseEnterCounter * 10)}%)`;
+                        break;
+                    case 10:
+                        
+                        gridItem.style.filter = `brightness(${100 - (mouseEnterCounter * 10)}%)`;
+                        console.log(mouseEnterCounter);
+
+                    default:
+                        mouseEnterCounter = Math.min(mouseEnterCounter, 10);
+
+                }
+               
             });
         
             gridContainer.appendChild(gridItem);
-            console.log(gridContainer);
         }
 
     }
